465c14a1eb7e6719568d5fa94da1fb6e39da9ba4
Kevin-Now <632728131@qq.com>
2025-08-30 10:24:31 +0800

    准备部署到GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/rijiapp/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
